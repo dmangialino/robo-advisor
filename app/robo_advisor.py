ad70fc203b162cd17d45dd4ea97fb756e97e364f
Dina Mangialino <dam566@stern.nyu.edu>
2021-06-10 20:14:32 -0400

    Fetch data from the API

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,1 +1,14 @@
-print("TO DO: make a web request or some stock market data.")
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv() 
+
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+symbol = "MSFT" # ask for a user input
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+response = requests.get(request_url)
+
+print(type(response))
+print(response.text)
