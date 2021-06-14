516d555c1775f54dad85b1a5134c030ece6b5f59
Dina Mangialino <dam566@stern.nyu.edu>
2021-06-14 18:08:32 -0400

    Print latest close, calculate and print recent high and low prices

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -11,6 +11,10 @@ load_dotenv()
 ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 symbol = ""
 
+# Program utilizes to_usd function provided by Professor Rossetti to convert values to USD format
+def to_usd(my_price):
+    return f"${my_price:,.2f}"
+
 while(True):
     symbol = input("Please provide a stock or cryptocurrency symbol: ")
     contains_digit = False
@@ -44,6 +48,9 @@ if "Error Message" in response.text:
     print("We're sorry, there was an error fetching data from the API. Please re-run the program to try again.")
     sys.exit()
 
+
+########## WRITE HISTORICAL DATA TO CSV ##########
+
 # Convert response from API to a dictionary
 parsed_response = json.loads(response.text)
 
@@ -53,29 +60,47 @@ response_df = DataFrame(parsed_response)
 # Create formatted DataFrame to write to CSV
 
 # Write historical data to local CSV file
+### TO DO: FIX FORMATTING?????
 csv_file_path = "data/prices.csv"
 response_df.to_csv(csv_file_path)
 
-# Calculate latest closing price
 
-# Caluculate recent high price
+########## CALCULATE REQUIRED FIELDS ##########
 
-# Calculate recent low price
+# Create list of dates
+timeseries = parsed_response["Time Series (Daily)"]
+
+# Calculate latest closing price (clos price on latest available day of data)
+# Utilized W3Schools documentation for sort function / reverse parameter (https://www.w3schools.com/python/ref_list_sort.asp)
+dates = list(timeseries.keys())
+dates.sort(reverse = True)
+latest_date = dates[0]
+latest_close = to_usd(float(timeseries[latest_date]["4. close"]))
+
+# Caluculate recent high price (max of high prices over past 100 days of trading data)
+# Calculate recent low price (min of low prices over past 100 days of trading data)
+high_prices = []
+low_prices = []
+for date in dates:
+    high_prices.append(timeseries[date]["2. high"])
+    low_prices.append(timeseries[date]["3. low"])
+
+high_price = to_usd(float(max(high_prices)))
+low_price = to_usd(float(min(low_prices)))
 
 # Print stock information and recommendation for user
 print("----------------------------------------")
 print("REQUESTED STOCK MARKET DATA...")
-print("REQUESTED AT: ", timestampStr)
+print("REQUESTED AT:", timestampStr)
 print("----------------------------------------")
-print("STOCK SYMBOL: ", symbol_formatted)
+print("STOCK SYMBOL:", symbol_formatted)
 print("----------------------------------------")
 print("LAST REFRESH:", response_df["Meta Data"]["3. Last Refreshed"])
-print("LATEST CLOSING PRICE: ")
-print("RECENT HIGH PRICE: ")
-print("RECENT LOW PRICE: ")
+print("LATEST CLOSING PRICE:", latest_close)
+print("RECENT HIGH PRICE:", high_price)
+print("RECENT LOW PRICE:", low_price)
 print("----------------------------------------")
-print("RECOMMENDATION: ")
-print("CONFIDENCE: ")
-print("RECOMMENDATION EXPLANATION: ")
+print("RECOMMENDATION:")
+print("CONFIDENCE:")
+print("RECOMMENDATION EXPLANATION:")
 print("----------------------------------------")
-
